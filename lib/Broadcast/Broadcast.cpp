da98f5c5d93a7ad94259470cd0414cbb5c45dba5
aaaasmile <testhaus@gmx.at>
2020-12-01 01:35:40 +0100

    send one line with wifi

--- lib/Broadcast/Broadcast.cpp
+++ lib/Broadcast/Broadcast.cpp
@@ -5,13 +5,13 @@
 #include <WiFi.h>
 #endif
 
-
-
 #include "Broadcast.h"
 #include "secret.txt"
 
 const char *ssid = STASSID;
 const char *password = STAPSK;
+const char *host = HOSTFORCONN;
+const int port = PORTFORCONN;
 
 Broadcast::Broadcast()
 {
@@ -55,4 +55,59 @@ void Broadcast::Setup()
 
 void Broadcast::SendData(String data)
 {
+    WiFiClient httpClient;
+    httpClient.setTimeout(7000);
+    int r = 0; //retry counter
+    int max_retry = 3;
+    while ((!httpClient.connect(host, port)) && (r < max_retry))
+    {
+        delay(100);
+        Serial.print(".");
+        r++;
+    }
+    if (r == max_retry)
+    {
+        Serial.println("Connection failed");
+        return;
+    }
+    else
+    {
+        Serial.println("Connected to web");
+    }
+    Serial.print("requesting URL: ");
+    Serial.println(host);
+
+    String getData, Link;
+
+    //POST Data
+    Link = "/iot/PubData";
+
+    httpClient.print(String("POST ") + Link + " HTTP/1.1\r\n" +
+                     "Host: " + host + "\r\n" +
+                     "Content-Type: application/x-www-form-urlencoded" + "\r\n" +
+                     "Content-Length: " + String(data.length()) + "\r\n\r\n" +
+                     data + "\r\n" +
+                     "Connection: close\r\n\r\n");
+    Serial.println("request sent");
+
+    while (httpClient.connected())
+    {
+        String line = httpClient.readStringUntil('\n');
+        if (line == "\r")
+        {
+            Serial.println("headers received");
+            break;
+        }
+    }
+
+    Serial.println("reply was:");
+    Serial.println("==========");
+    String line;
+    while (httpClient.available())
+    {
+        line = httpClient.readStringUntil('\n'); //Read Line by Line
+        Serial.println(line);                    //Print response
+    }
+    Serial.println("==========");
+    Serial.println("closing connection");
 }
